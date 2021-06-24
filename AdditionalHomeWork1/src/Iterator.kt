29f16e6b9935d76d6d22f116c7bcc319cda8bcb1
vitaliy-anatolievych <vitaliy-anatolievych@gmail.com>
2021-06-24 12:00:58 +0300

    task is done

--- AdditionalHomeWork1/src/Iterator.kt
+++ AdditionalHomeWork1/src/Iterator.kt
@@ -3,10 +3,7 @@ class Iterator(val lists: List<List<Int>>) {
     var countOfElements: Int = 0
     var iteration = 0
 
-    var iterationElement = 0
-    var iterationList = 0
-
-    var mayBeSortedList: List<Int> = mutableListOf()
+    var mayBeSortedList = mutableListOf<Int>()
 
     init {
 
@@ -15,43 +12,22 @@ class Iterator(val lists: List<List<Int>>) {
             countOfElements += lists[currentList].size
             for (num in lists.get(currentList).indices) {
                 val currentNum = lists.get(currentList).get(num)
-                // я хочу увидеть тут метод mayBeSortedList.add(currentNum) бляха муха
+                // сразу же запоминаем ОДИН РАЗ И НАВСЕГДА НА ВСЮ ЖИЗНЬ ЭТОГО КЛАССА НАД КОТОРЫМ Я ПОМУЧИЛСЯ этот список для дальнейшей сортировки
+                mayBeSortedList.add(currentNum)
             }
         }
+        mayBeSortedList.sort()
     }
 
     fun hasNext(): Boolean {
         // ...
         if (iteration == countOfElements) isListNotEnded = false
-        iteration++
         return isListNotEnded
     }
 
     //@switchingList - определяет необходимость перейти на следующий список
     fun next(): Int {
         // ...
-        switchingList()
-        return sortManager()
+        return mayBeSortedList.get(iteration++)
     }
-
-    // @sortManager отдаёт отсортированное число
-    private fun sortManager(): Int {
-//        lists.get(iterationList).get(iterationElement++)
-        println(mayBeSortedList.toString())
-        return 0
-    }
-
-    //@resetIteratorElement() если мы достигли конца списка - обнуляем итератор елементов
-    private fun switchingList() {
-        if (iterationElement == lists.get(iterationList).size) {
-            iterationList++
-            resetIteratorElement()
-        }
-    }
-
-    private fun resetIteratorElement() {
-        iterationElement = 0
-    }
-
-
 }
