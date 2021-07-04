5f529c625b72cc898d6af54f7656d293fa3124da
vitaliy-anatolievych <vitaliy-anatolievych@gmail.com>
2021-07-04 19:23:19 +0300

    Home Work 3 Done

--- HomeWorkLesson3/app/src/main/java/com/vitalii/myapplication/CalendarActivity.kt
+++ HomeWorkLesson3/app/src/main/java/com/vitalii/myapplication/CalendarActivity.kt
@@ -29,19 +29,18 @@ class CalendarActivity : AppCompatActivity() {
         val validateBtn = findViewById<Button>(R.id.validate_btn)
         val resultTxt = findViewById<TextView>(R.id.result)
         validateBtn.setOnClickListener {
-            Log.i("Input", "${dateEditText.text}")
             arrayDate.addAll(dateEditText.text.split("\n"))
-            Log.i("Array", Arrays.toString(arrayDate.toArray()))
 
             if (dateEditText.text.isEmpty()) {
                 resultTxt.text = "Поле пустое"
                 resultTxt.setTextColor(Color.RED)
             } else {
-                if (calculateResult(arrayDate)) {
-                    resultTxt.text = "Прибывание разрешено"
+                val daysInEU = calculateResult(arrayDate)
+                if (daysInEU < 90) {
+                    resultTxt.text = getString(R.string.days_left, 90 - daysInEU)
                     resultTxt.setTextColor(Color.GREEN)
                 } else {
-                    resultTxt.text = "Прибывание запрещенно"
+                    resultTxt.text = getString(R.string.alert_days, 90 - daysInEU)
                     resultTxt.setTextColor(Color.RED)
                 }
             }
@@ -49,24 +48,58 @@ class CalendarActivity : AppCompatActivity() {
     }
 
     @SuppressLint("SimpleDateFormat")
-    private fun calculateResult(arrayDate: ArrayList<String>): Boolean {
-        var summaryDay: Long = 0
-
+    private fun calculateResult(arrayDate: ArrayList<String>): Long {
+        var timeSpentInTheEU: Long = 0
 
         for (date in arrayDate.indices) {
-            val dateStart: String = arrayDate[date].substringAfter("-").trim()
-            val dateEnd: String = arrayDate[date].substringBefore("-").trim()
+            val dateOfEntry = arrayDate.get(date).substringBefore("-").trim()
+            val dateOfLeave = arrayDate.get(date).substringAfter("-").trim()
+            val dateSixMonthAgo = findTheDateSixMonthsAgo(dateOfLeave)
 
-            val formatDateStart: Date? = SimpleDateFormat("dd.MM.yyyy").parse(dateStart)
-            val formatDateEnd: Date? = SimpleDateFormat("dd.MM.yyyy").parse(dateEnd)
-            Log.i("Date", "$formatDateStart | $formatDateEnd")
 
-            val differenceDate: Long = (formatDateEnd?.time ?: 0) - (formatDateStart?.time ?: 0)
-            summaryDay += differenceDate / (1000 * 60 * 60 * 24)
+            timeSpentInTheEU = calculateDays(dateSixMonthAgo, dateOfLeave, arrayDate)
+
         }
 
+        return timeSpentInTheEU
+    }
 
-        Log.i("Summary", "$summaryDay")
-        return false
+
+    /**
+     * Метод который берёт отрезок начальная точка: @dateSixMonthAgo конечная точка @dateOfLeave
+     * Цель: найти даты въезда-выезда в ЕС в этом интервале и посчитать количество проведённых дней
+     */
+    private fun calculateDays(dateSixMonthAgo: String, dateOfLeave: String,  arrayDate: List<String>): Long {
+        val formatDaySixMonthAgo = SimpleDateFormat("dd.MM.yyyy").parse(dateSixMonthAgo)
+        val formatDayLeave = SimpleDateFormat("dd.MM.yyyy").parse(dateOfLeave)
+
+        var summaryDays: Long = 0
+
+        for (date in arrayDate.indices) {
+            val currentDateOfEntry = SimpleDateFormat("dd.MM.yyyy").parse(arrayDate.get(date).substringBefore("-").trim())
+            val currentDateOfLeave = SimpleDateFormat("dd.MM.yyyy").parse(arrayDate.get(date).substringAfter("-").trim())
+
+            if (currentDateOfEntry.time > formatDaySixMonthAgo.time && currentDateOfEntry.time < formatDayLeave.time) {
+                summaryDays += (currentDateOfLeave.time - currentDateOfEntry.time) / (1000 * 60 * 60 * 24)
+            }
+        }
+
+        return summaryDays
+    }
+
+    /**
+     * Метод поиска даты которая была пол года назад
+     * @endOfStayInEU - Дата -180 дней от дня въезда в миллисекундах
+     * @calendar - форматирование в привычную дату
+     * @return возвращаем дату в виде текста
+     */
+    private fun findTheDateSixMonthsAgo(dateOfLeave: String): String {
+        val dateFormat = SimpleDateFormat("dd.MM.yyyy")
+        val endOfStayInEU = dateFormat.parse(dateOfLeave).time - 1.555e+10
+
+        val calendar = Calendar.getInstance()
+        calendar.timeInMillis = endOfStayInEU.toLong()
+
+        return dateFormat.format(calendar.timeInMillis)
     }
 }
