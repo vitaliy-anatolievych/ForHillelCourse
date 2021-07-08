c572281d71df72ce1d98fec3ba49d48904c6a905
vitaliy-anatolievych <vitaliy-anatolievych@gmail.com>
2021-07-08 18:49:14 +0300

    not complete

--- HomeWorkLesson4/src/Iterator.kt
+++ HomeWorkLesson4/src/Iterator.kt
@@ -1,25 +1,31 @@
 class Iterator(val lists: List<List<Int>>) {
-    private var isEndedElement: Boolean = false
+    var isListNotEnded = true
+    var countOfElements: Int = 0
+    var iteration = 0
 
+    var mayBeSortedList = mutableListOf<Int>()
 
+    init {
+
+        // Я хочу узнать сколько у меня в целом содержится элементов во всех списках
+        for (currentList: Int in lists.indices) {
+            countOfElements += lists[currentList].size
+            for (num in lists.get(currentList).indices) {
+                val currentNum = lists.get(currentList).get(num)
+                mayBeSortedList.add(currentNum)
+            }
+        }
+        mayBeSortedList.sort()
+    }
 
     fun hasNext(): Boolean {
         // ...
-        return !isEndedElement
+        if (iteration == countOfElements) isListNotEnded = false
+        return isListNotEnded
     }
 
     fun next(): Int {
         // ...
-        var currentNumber = lists[0][0]
-
-        lists.get(0).forEach {
-            if (it == currentNumber) {
-                return currentNumber
-            }
-        }
-        
-        isEndedElement = true
-        return currentNumber
+        return mayBeSortedList.get(iteration++)
     }
-
 }
